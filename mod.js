ff5f0e448f5124e6e67520a85a945eedd61d5278
Name2781 <98187590+Name2781@users.noreply.github.com>
2022-01-24 13:16:02 -0500

    Update mod.js

--- mod.js
+++ mod.js
@@ -12,13 +12,19 @@ function onLoad() {
   h.class="modMenuDiv";
   h.innerHTML = html;
   document.body.appendChild(h);   
-  
+
   var c = document.createElement('link');
   c.rel = 'stylesheet';
   c.media = 'all';
   c.href = 'https://name2781.github.io/moreore/test.css';
   c.type = 'text/css';
-  document.getElementsByTagName('head')[0].appendChild(c); 
+  document.getElementsByTagName('head')[0].appendChild(c);
+  var b = document.createElement('link');
+  b.rel = 'stylesheet';
+  b.media = 'all';
+  b.href = 'code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css';
+  b.type = 'text/css';
+  document.getElementsByTagName('head')[0].appendChild(b);
   $('.modMenuDiv').draggable();
-  alert("worked");
+  // $('.modMenuDiv').resizable();
 }
